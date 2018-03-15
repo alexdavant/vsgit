11cc48dba46606565282ce2c19268750dc4595a9
alexdavant <alex.davant@gmail.com>
2018-03-15 12:27:16 +0100

    ok1

---
+++ TestGitHub1/Etudiant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TestGitHub1
+{
+    public class Etudiant
+    {
+        private string nom, prenom;
+        private int age;
+        public Etudiant()
+        {
+        }
+    }
+}
